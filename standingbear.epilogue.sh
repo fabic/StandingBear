9d7531ca3bfa4d05e0ac8252bd1136a5e481a170
fcj <fabi@RandomBinaryThoughts.com>
2013-10-23 16:50:07 +0200

    Copy-pasted c/o sample customizations from standingbear.epilogue.sh into standingbear.prologue.sh

--- standingbear.epilogue.sh
+++ standingbear.epilogue.sh
@@ -3,7 +3,11 @@
 #
 # This file is sourced *late* by standingbear.sh, further
 # environment configuration may be done here, such as adding
-# custom environment variable names to the $Environment bash array.
+# custom environment variable names to the $Environment bash array that
+# depend on processing done by standingbear.sh
+#
+# Note that the best place for defining environment is still standingbear.prologue.sh
+# so that customization be mainly kept in one place.
 #
 
 # Sample custom env. var., defined in standingbear.prologue.sh :
@@ -13,6 +17,7 @@
 #SYBASE=${SYBASE:-/opt/sybase}
 #SYBASE_OCS=${SYBASE_OCS:-OCS-15_0}
 #DSQUERY=${DSQUERY:-}
+
 # From time to time I have to set it for dl to resolve Sybase libsyb*64.so, e.g. :
 #    « Cannot load /opt/php-5.3.23/lib/libphp5.so into server:
 #        libsybunic64.so: cannot open shared object file: No such file or directory »
--- standingbear.prologue.sh
+++ standingbear.prologue.sh
@@ -164,4 +164,21 @@ ApacheDefines=( "${ApacheDefines[@]}" AI_APAXY )
 ApacheDefines=( "${ApacheDefines[@]}" GIT_HTTP_BACKEND )
 ApacheDefines=( "${ApacheDefines[@]}" GITWEB )
 
+#######################################################################
+### Custom "to-be-passed" environment may be done from within here, ###
+### or in standingbear.epilogue.sh                                  ###
+#######################################################################
+
+## Sybase ASE 15.x :
+#SYBASE=${SYBASE:-/opt/sybase}
+#SYBASE_OCS=${SYBASE_OCS:-OCS-15_0}
+#DSQUERY=${DSQUERY:-}
+
+# From time to time I have to set it for dl to resolve Sybase libsyb*64.so, e.g. :
+#    « Cannot load /opt/php-5.3.23/lib/libphp5.so into server:
+#        libsybunic64.so: cannot open shared object file: No such file or directory »
+#pathprepend "$SYBASE/$SYBASE_OCS/lib" LD_LIBRARY_PATH
+
+#Environment=( "${Environment[@]}" SYBASE SYBASE_OCS DSQUERY )
+
 # vim: filetype=sh
